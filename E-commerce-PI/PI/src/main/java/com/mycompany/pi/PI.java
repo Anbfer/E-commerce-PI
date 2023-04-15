121bc7c8b3398d852e3cbb5de669be7df550db36
Angelo <92029180+Anbfer@users.noreply.github.com>
2023-04-15 19:24:01 -0300

    Atualização PI

--- E-commerce-PI/PI/src/main/java/com/mycompany/pi/PI.java
+++ E-commerce-PI/PI/src/main/java/com/mycompany/pi/PI.java
@@ -11,6 +11,6 @@ package com.mycompany.pi;
 public class PI {
 
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        new CadastroCliente().setVisible(true);
     }
 }
