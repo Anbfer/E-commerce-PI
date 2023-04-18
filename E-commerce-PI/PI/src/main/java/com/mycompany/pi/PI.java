3b9eb93b7bbc2340b4eadfbd371565bf784f5186
Ângelo Baracho <92029180+Anbfer@users.noreply.github.com>
2023-04-18 10:47:38 -0300

    Add files via upload
    Alterações de validação nos campos das telas. Alteração das regex para limitar o número de caracteres digitados nos campos.

--- E-commerce-PI/PI/src/main/java/com/mycompany/pi/PI.java
+++ E-commerce-PI/PI/src/main/java/com/mycompany/pi/PI.java
@@ -8,7 +8,7 @@ package com.mycompany.pi;
  *
  * @author angelo.bferreira
  */
-public class PI {
+public class PI extends Validadores {
 
     public static void main(String[] args) {
         new CadastroCliente().setVisible(true);
