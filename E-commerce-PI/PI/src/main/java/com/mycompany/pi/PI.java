991cc03d44cf13e0fd4dc2964697a08cd9e7670c
Ângelo Baracho <92029180+Anbfer@users.noreply.github.com>
2023-04-18 11:19:01 -0300

    Padronização das telas para o estilo Nimbus

--- E-commerce-PI/PI/src/main/java/com/mycompany/pi/PI.java
+++ E-commerce-PI/PI/src/main/java/com/mycompany/pi/PI.java
@@ -11,6 +11,20 @@ package com.mycompany.pi;
 public class PI extends Validadores {
 
     public static void main(String[] args) {
+
+        for (javax.swing.UIManager.LookAndFeelInfo info : javax.swing.UIManager
+                .getInstalledLookAndFeels()) {
+            if ("Nimbus".equals(info.getName())) {
+                try {
+                    javax.swing.UIManager.setLookAndFeel(info.getClassName());
+                    break;
+                } catch (ClassNotFoundException | InstantiationException | IllegalAccessException
+                        | UnsupportedLookAndFeelException ex) {
+                    Logger.getLogger(PI.class.getName()).log(Level.SEVERE, null, ex);
+                }
+            }
+        }
+
         new CadastroCliente().setVisible(true);
     }
 }
