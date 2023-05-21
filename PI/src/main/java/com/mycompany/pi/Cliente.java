bacf9001063dceffd5548cccde18bbbfcf9612e7
Ângelo Baracho <92029180+Anbfer@users.noreply.github.com>
2023-05-21 14:43:08 -0300

    Add files via upload

---
+++ PI/src/main/java/com/mycompany/pi/Cliente.java
@@ -0,0 +1,102 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.pi;
+
+/**
+ *
+ * @author Angelo
+ */
+public class Cliente {
+    private String id;
+    private String nome;
+    private String genero;
+    private String cpf;
+    private String email;
+    private String endereco;
+    private String telefone;
+    
+    public Cliente() {
+    }
+    
+    
+    public Cliente(String id, String nome, String genero, String cpf, String email, String endereco, String telefone) {
+        this.id = id;
+        this.nome = nome;
+        this.genero = genero;
+        this.cpf = cpf;
+        this.email = email;
+        this.endereco = endereco;
+        this.telefone = telefone;
+    }
+
+    public Cliente(String nome, String genero, String cpf, String email, String endereco, String telefone) {
+        this.nome = nome;
+        this.genero = genero;
+        this.cpf = cpf;
+        this.email = email;
+        this.endereco = endereco;
+        this.telefone = telefone;
+    }
+    
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getGenero() {
+        return genero;
+    }
+
+    public void setGenero(String genero) {
+        this.genero = genero;
+    }
+
+    public String getCpf() {
+        return cpf;
+    }
+
+    public void setCpf(String cpf) {
+        this.cpf = cpf;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getEndereco() {
+        return endereco;
+    }
+
+    public void setEndereco(String endereco) {
+        this.endereco = endereco;
+    }
+
+    public String getTelefone() {
+        return telefone;
+    }
+
+    public void setTelefone(String telefone) {
+        this.telefone = telefone;
+    }
+    
+    
+    
+}
